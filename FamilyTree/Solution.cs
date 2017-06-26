6fc391943928f28762771e5f2d3e59500a7406a3
Alex Saber <alexsaber89@gmail.com>
2017-06-26 14:40:20 -0500

    possible solution implemented, though unit test still not passing

--- FamilyTree/Solution.cs
+++ FamilyTree/Solution.cs
@@ -8,11 +8,25 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
-            //foreach (var descendant in person.Descendants)
-            //{
+            if (person.Name == descendantName)
+            {
+                return person.Birthday.ToString("MMMM");
+            }
 
-            //}
+            foreach (var descendant in person.Descendants)
+            {
+                if (descendant.Name == descendantName)
+                {
+                    return descendant.Birthday.ToString("MMMM");
+                }
+            }
+
+            foreach (var descendant in person.Descendants)
+            {
+                GetBirthMonth(descendant, descendantName);
+            }
+
+            return "";
         }
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -10,8 +10,8 @@ namespace UnitTestProject1
         [TestMethod]
         public void if_the_person_exists_in_tree_the_result_should_be_january()
         {
-            var person = FamilyTreeGenerator.Make();
-            var result = new Solution().GetBirthMonth(person, "Joe");
+            var rootNode = FamilyTreeGenerator.Make();
+            var result = new Solution().GetBirthMonth(rootNode, "Joe");
 
             Assert.AreEqual("January", result);
         }
@@ -19,8 +19,8 @@ namespace UnitTestProject1
         [TestMethod]
         public void if__the_person_exists_at_the_top_tree_the_result_should_be_may()
         {
-            var person = FamilyTreeGenerator.Make();
-            var result = new Solution().GetBirthMonth(person, "Ted");
+            var rootNode = FamilyTreeGenerator.Make();
+            var result = new Solution().GetBirthMonth(rootNode, "Ted");
 
             Assert.AreEqual("May", result);
         }
@@ -28,8 +28,8 @@ namespace UnitTestProject1
         [TestMethod]
         public void if_the_person_does_not_exist_in_the_tree_the_result_should_be_empty()
         {
-            var person = FamilyTreeGenerator.Make();
-            var result = new Solution().GetBirthMonth(person, "Jeebus");
+            var rootNode = FamilyTreeGenerator.Make();
+            var result = new Solution().GetBirthMonth(rootNode, "Jeebus");
 
             Assert.AreEqual("", result);
         }
